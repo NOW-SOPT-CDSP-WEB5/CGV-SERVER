187167424efb4839933074160dc9f4fe5830c60c
kgy1008 <gayeon1008@g.skku.edu>
2024-05-16 00:56:10 +0900

    [feat] #14 isTicketed 추가

--- src/main/java/org/sopt/cgv/service/dto/MovieDetailRequestDto.java
+++ src/main/java/org/sopt/cgv/service/dto/MovieDetailRequestDto.java
@@ -5,9 +5,10 @@ import org.sopt.cgv.domain.Movie;
 public record MovieDetailRequestDto (
         String title,
         String script,
-        boolean isLiked
+        boolean isLiked,
+        boolean isticketd
 ) {
-    public static MovieDetailRequestDto of(Movie movie, boolean isLiked) {
-        return new MovieDetailRequestDto(movie.getTitle(), movie.getScript(), isLiked);
+    public static MovieDetailRequestDto of(Movie movie, boolean isLiked, boolean isTicketd) {
+        return new MovieDetailRequestDto(movie.getTitle(), movie.getScript(), isLiked, isTicketd);
     }
 }

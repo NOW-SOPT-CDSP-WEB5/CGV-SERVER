e487049298738609f482eaae870945bccceb0ad5
kgy1008 <gayeon1008@g.skku.edu>
2024-05-16 00:06:24 +0900

    [feat] #12 theater 엔티티 구현

---
+++ src/main/java/org/sopt/cgv/domain/Theater.java
@@ -0,0 +1,31 @@
+package org.sopt.cgv.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Theater {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String theaterName;
+
+    private int maxSeats;
+
+    private String theaterType;
+
+    private LocalTime startAt;
+
+    private LocalTime endAt;
+
+    private int remainingSeats;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+}

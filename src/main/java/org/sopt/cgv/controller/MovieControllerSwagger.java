513967a2aa147f81db22aa54b749e20421e6c22a
kgy1008 <gayeon1008@g.skku.edu>
2024-05-15 20:04:17 +0900

    [feat] #4 Swagger 설정

---
+++ src/main/java/org/sopt/cgv/controller/MovieControllerSwagger.java
@@ -0,0 +1,25 @@
+package org.sopt.cgv.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.sopt.cgv.service.dto.MovieDetailRequestDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Tag(name = "영화 API", description = "영화 관련 API")
+public interface MovieControllerSwagger {
+
+    @Operation(summary = "영화 세부 정보 조회 API")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = MovieDetailRequestDto.class))),
+            }
+    )
+    ResponseEntity<MovieDetailRequestDto> getMovieDetail(@PathVariable Long movieId);
+}

b2c788a4eddd1522f713a37b1af3cfea04cedf82
tkdwns414 <sangjune2000@naver.com>
2024-05-19 04:53:37 +0900

    [feat] #20 remove unused import statement

--- src/main/java/org/sopt/cgv/service/TheaterService.java
+++ src/main/java/org/sopt/cgv/service/TheaterService.java
@@ -1,15 +1,12 @@
 package org.sopt.cgv.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.sopt.cgv.domain.Theater;
 import org.sopt.cgv.dto.TheaterTopDto;
 import org.sopt.cgv.repository.TheaterRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TheaterService {

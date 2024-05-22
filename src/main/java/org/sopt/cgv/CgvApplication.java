b56851bbcb81aa9f83ee4abb3595e6e598204b6c
kgy1008 <gayeon1008@g.skku.edu>
2024-05-22 17:55:05 +0900

    [fix] #26 Swagger Cors 에러 해결

--- src/main/java/org/sopt/cgv/CgvApplication.java
+++ src/main/java/org/sopt/cgv/CgvApplication.java
@@ -1,8 +1,11 @@
 package org.sopt.cgv;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://www.now-sopt-cgv.n-e.kr/")})
 @SpringBootApplication
 public class CgvApplication {
 
